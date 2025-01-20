632be2d82199eea5e13ac6aab797dbc5b23a74bc
bernie-cm <bernie.camejo@icloud.com>
2025-01-20 17:45:59 +1100

    Edited app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
-    return jsonify({'message': 'wello, World!'})
+    return jsonify({"message": "Hello, World!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
